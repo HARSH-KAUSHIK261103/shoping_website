7e98c5ba369fa39b71e4b330c68bb60f5ad873ad
Harsh Kaushik <148998891+HARSH-KAUSHIK261103@users.noreply.github.com>
2024-08-14 20:58:14 +0530

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,38 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { Product, FooterBanner, HeroBanner } from "../components";
+
+import { client } from "../sanity/lib/client";
+
+const Home = ({ products, bannerData }) => (
+  <div>
+    <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+    <div className="products-heading">
+      <h2>Best Seller Products</h2>
+      <p>speaker There are many variations passages</p>
+    </div>
+
+    <div className="products-container">
+      {products?.map((product) => (
+        <Product key={product._id} product={product} />
+      ))}
+    </div>
+
+    <FooterBanner footerBanner={bannerData && bannerData[1]} />
+  </div>
+);
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
+};
+
+export default Home;
---
+++ sanity/env.js
@@ -0,0 +1,8 @@
+export const apiVersion =
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-02-26";
+
+export const dataset = " ";
+export const projectId = " ";
+export const useCdn = false;
+export const token =
+  " ";
